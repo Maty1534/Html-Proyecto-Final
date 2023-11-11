1c9023d13d012903056374ca09e1c287d6577333
Matias Emanuel Correa <correamatias.0922@gmail.com>
2023-11-11 15:53:37 -0300

    se arreglo el media query de 768px

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 document.querySelector(".bars__menu").addEventListener("click", animateBars);
 
-const navbar = document.querySelector('.nav__links');
+var navbar = document.querySelector('.nav__links');
 var line1__bars = document.querySelector(".line1__bars-menu");
 var line2__bars = document.querySelector(".line2__bars-menu");
 var line3__bars = document.querySelector(".line3__bars-menu");
