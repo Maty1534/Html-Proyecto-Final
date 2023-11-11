aed25c95807ecaf76ebdb8f4f06de035d493a060
Matias Emanuel Correa <correamatias.0922@gmail.com>
2023-11-11 00:26:49 -0300

    un buen header realizado

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 document.querySelector(".bars__menu").addEventListener("click", animateBars);
 
+const navbar = document.querySelector('.navbar');
 var line1__bars = document.querySelector(".line1__bars-menu");
 var line2__bars = document.querySelector(".line2__bars-menu");
 var line3__bars = document.querySelector(".line3__bars-menu");
@@ -8,4 +9,6 @@ function animateBars() {
     line1__bars.classList.toggle("activeline1__bars-menu");
     line2__bars.classList.toggle("activeline2__bars-menu");
     line3__bars.classList.toggle("activeline3__bars-menu");
+    navbar.style.display = (navbar.style.display === 'flex') ? 'none' : 'flex';
 }
+  
