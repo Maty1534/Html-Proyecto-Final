e5b1d86256a99a2b2b290439e7bdc6371064888f
Matias Emanuel Correa <correamatias.0922@gmail.com>
2023-11-21 19:02:17 -0300

    Ultimos toques con las fuentes y tamaños de multimedia

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 document.querySelector(".bars__menu").addEventListener("click", animateBars);
 
-var navbar = document.querySelector('.nav__links');
+var navbar = document.querySelector(".nav__links");
 var line1__bars = document.querySelector(".line1__bars-menu");
 var line2__bars = document.querySelector(".line2__bars-menu");
 var line3__bars = document.querySelector(".line3__bars-menu");
+var body = document.querySelector("body");
 
 function animateBars() {
-    line1__bars.classList.toggle("activeline1__bars-menu");
-    line2__bars.classList.toggle("activeline2__bars-menu");
-    line3__bars.classList.toggle("activeline3__bars-menu");
-    navbar.classList.toggle("nav__links--open");
+  line1__bars.classList.toggle("activeline1__bars-menu");
+  line2__bars.classList.toggle("activeline2__bars-menu");
+  line3__bars.classList.toggle("activeline3__bars-menu");
+  navbar.classList.toggle("nav__links--open");
+  body.classList.toggle("no-scroll");
 }
-  
